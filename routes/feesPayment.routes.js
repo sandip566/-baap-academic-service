b394054d2fb68be172a14e025faa658b878cb56e
kandalkarpriyanka99 <kandalkarpriyanka99@.com>
2024-01-13 11:20:07 +0530

    update code

--- routes/feesPayment.routes.js
+++ routes/feesPayment.routes.js
@@ -19,27 +19,22 @@ router.post(
     requestResponsehelper.sendResponse(res, serviceResponse);
   }
 );
-
 router.delete("/:id", async (req, res) => {
   const serviceResponse = await service.deleteById(req.params.id);
   requestResponsehelper.sendResponse(res, serviceResponse);
 });
-
 router.put("/:id", async (req, res) => {
   const serviceResponse = await service.updateById(req.params.id, req.body);
   requestResponsehelper.sendResponse(res, serviceResponse);
 });
-
 router.get("/:id", async (req, res) => {
   const serviceResponse = await service.getById(req.params.id);
   requestResponsehelper.sendResponse(res, serviceResponse);
 });
-
 router.get("/all/FeesPayment", async (req, res) => {
   const serviceResponse = await service.getAllByCriteria({});
   requestResponsehelper.sendResponse(res, serviceResponse);
-});
-
+});  
 router.get("/getAllFeesPayment/groupId/:groupId", async (req, res) => {
   const groupId = req.params.groupId;
   const criteria = {
