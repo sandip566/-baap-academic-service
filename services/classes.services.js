2265c8dcec58a2cb00af2bc2b68ddd38fbaff025
nileshgudaghe2004 <nileshgudaghe2004@gmail.com>
2024-01-17 11:10:49 +0530

    Created classes api

---
+++ services/classes.services.js
@@ -0,0 +1,41 @@
+const ClassModel = require("../schema/classes.schema");
+const BaseService = require("@baapcompany/core-api/services/base.service");
+
+class ClassService extends BaseService {
+    constructor(dbModel, entityName) {
+        super(dbModel, entityName);
+    }
+
+    getAllDataByGroupId(groupId, criteria) {
+        const query = {
+            groupId: groupId,
+        };
+        if (criteria.name) query.name = new RegExp(criteria.name, "i");
+        if (criteria.location) query.location = new RegExp(criteria.location, "i");
+        if (criteria.phone) query.phone = new RegExp(criteria.phone);
+        return this.preparePaginationAndReturnData(query, criteria);
+    }
+
+    async deleteClassById(classId, groupId) {
+        try {
+            return await ClassModel.deleteOne({ _id: classId, groupId: groupId });
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateClassById(classId, groupId, newData) {
+        try {
+            const updateClass = await ClassModel.findOneAndUpdate(
+                { _id: classId, groupId: groupId },
+                newData,
+                { new: true }
+            );
+            return updateClass;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+module.exports = new ClassService(ClassModel, "class");
