b992b83e9de581103e162913ed1851d8c01ca736
gaurikhemnar <gaurikhemnar@gmail.com>
2024-01-22 14:25:31 +0530

    Correct code

--- schema/courses.schema.js
+++ schema/courses.schema.js
@@ -27,75 +27,11 @@ const courseSchema = new mongoose.Schema(
             required:true
          },
         Fees: {
-             type: mongoose.Schema.Types.ObjectId,
-             autopopulate: true,
-             ref:'feesTemplate',
-         },
-       
-        // numberOfSemister: {
-        //     type: Number,
-        //     require: true
-        // },
-        // maxixmumAllowedStudent: {
-        //     type: Number,
-        //     require: true
-        // },
-        // abbrivation: {
-        //     type: String,
-        //     require: true
-        // },
-        // provider: {
-        //     type: String,
-        //     require: true
-        // },
-        // courseFormat: {
-        //     type: String,
-        //     require: true
-        // },
-        // academicYear: {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     require: true,
-        //     autopopulate: true,
-        //     ref: 'academicyear'
-        // },
-        // division: {
-        //     type: [mongoose.Schema.Types.ObjectId],
-        //     autopopulate: true,
-        //     ref: 'division'
-        // },
-        // subjects: {
-        //     sem1: {
-        //         type: [mongoose.Schema.Types.ObjectId],
-        //         autopopulate: true,
-        //         ref: 'subject'
-        //     },
-        //     sem2: {
-        //         type: [mongoose.Schema.Types.ObjectId],
-        //         autopopulate: true,
-        //         ref: 'subject'
-        //     },
-        //     sem3: {
-        //         type: [mongoose.Schema.Types.ObjectId],
-        //         autopopulate: true,
-        //         ref: 'subject'
-        //     },
-        //     sem4: {
-        //         type: [mongoose.Schema.Types.ObjectId],
-        //         autopopulate: true,
-        //         ref: 'subject'
-        //     },
-        //     sem5: {
-        //         type: [mongoose.Schema.Types.ObjectId],
-        //         autopopulate: true,
-        //         ref: 'subject'
-        //     },
-        //     sem6: {
-        //         type: [mongoose.Schema.Types.ObjectId],
-        //         autopopulate: true,
-        //         ref: 'subject'
-        //     }
-        // }
-
+            //  type: mongoose.Schema.Types.ObjectId,
+            //  autopopulate: true,
+            //  ref:'feesTemplate',
+            type:Number
+         }, 
     },
     { strict: false, timestamps: true }
 );
