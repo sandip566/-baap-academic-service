0a1a7045d5aaf1bdcea60a89f2f6134b898fa918
Akshada khatode <akshadakhatode@gmail.com>
2024-02-07 12:34:16 +0530

    change toke base service functions

--- services/token.services.js
+++ services/token.services.js
@@ -3,13 +3,33 @@ const jwt = require('jsonwebtoken');
 class TokenService {
 
     static isAdmin(decodedToken) {
-        return decodedToken.role && decodedToken.role.name.includes("admin");
+        return decodedToken.role && decodedToken.role.name.toLowerCase().includes("admin");
     }
-
+    
+    static getIndividualPermissions(decodedToken) {
+        const permissionsArray = decodedToken.role.permissions.reduce((acc, permission) => {
+            acc.push(...permission.split(','));
+            return acc;
+        }, []);
+        
+        return permissionsArray;
+    }
+    
     static hasAllowedAction(decodedToken, allowedActions) {
-        return decodedToken.role && allowedActions.some(action => decodedToken.role.permissions.includes(action));
+        const individualPermissions = TokenService.getIndividualPermissions(decodedToken);
+        console.log("individualPermissions", individualPermissions);
+        
+        let decodedTokenData = decodedToken.role && allowedActions.every(actions => {
+            const permissions = actions.split(',');
+            return permissions.every(permission => individualPermissions.includes(permission.trim()));
+        });
+    
+        console.log("allow Actions", allowedActions);
+        console.log("decodedTokenData", decodedTokenData);
+        
+        return decodedTokenData;
     }
-
+    
     static isAuthenticated(req) {
         const token = req.headers.authorization;
         return !!token;
@@ -19,7 +39,7 @@ class TokenService {
         const { userId, name, phoneNumber, servicerequestId, year, month, orderId, categoryId, status, search } = req.query;
         return (userId || name || phoneNumber || servicerequestId || year || orderId || month || categoryId || status || search);
     }
-
+   
     static checkPermission(allowedActions) {
         return async (req, res, next) => {
             if (!TokenService.isAuthenticated(req)) {
@@ -28,16 +48,19 @@ class TokenService {
 
             try {
                 const decodedToken = await TokenService.decodeToken(req.headers.authorization);
+                console.log("decodedToken", decodedToken);
                 const hasPermission = TokenService.isAdmin(decodedToken)
-                    ? TokenService.hasAllowedAction(decodedToken, allowedActions)
-                    : TokenService.hasAllowedAction(decodedToken, allowedActions) && TokenService.checkQueryParams(req);
-
+                ? TokenService.hasAllowedAction(decodedToken, allowedActions)
+                : TokenService.hasAllowedAction(decodedToken, allowedActions) && TokenService.checkQueryParams(req);
+            
+            console.log("hasPermission",hasPermission);
                 if (hasPermission) {
                     next();
                 } else {
                     return res.status(403).json({ message: "You do not have permission to Perform this Action" });
                 }
             } catch (error) {
+                console.log(error);
                 return res.status(500).json({ message: "Token verification failed" });
             }
         };
