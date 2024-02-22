cfd66b1594f75ac5beec4b17e07f2d46c540d423
Vaibhav Nehe <90456257+vaibhavnehe70@users.noreply.github.com>
2024-02-22 13:14:40 +0530

    changed feesDetail to feesDetails

--- services/feesInstallment.services.js
+++ services/feesInstallment.services.js
@@ -259,7 +259,7 @@ class feesInstallmentService extends BaseService {
                     $match: {
                         "courseDetails.class_id": Number(classId),
                         groupId: Number(groupId),
-                        "feesDetail.feesTemplateId": Number(feesTemplateId),
+                        "feesDetails.feesTemplateId": Number(feesTemplateId),
                         academicYear: academicYear
                     }
                 },
