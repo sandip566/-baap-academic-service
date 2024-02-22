6eb3946d1af6753d88b53ca20dedf68d3a2f92ac
Vaibhav Nehe <90456257+vaibhavnehe70@users.noreply.github.com>
2024-02-22 13:00:14 +0530

    bug fix get total fees and pending fees

--- services/feesInstallment.services.js
+++ services/feesInstallment.services.js
@@ -167,7 +167,7 @@ class feesInstallmentService extends BaseService {
                     $match: {
                         "courseDetails.course_id": Number(courseId),
                         groupId: Number(groupId),
-                        "feesDetail.feesTemplateId": Number(feesTemplateId),
+                        "feesDetails.feesTemplateId": Number(feesTemplateId),
                         academicYear: academicYear
                     }
                 },
