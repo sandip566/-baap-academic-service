f25f656fdeb33477d2ed6312231607f6dace128d
Akshada khatode <akshadakhatode@gmail.com>
2024-02-23 16:09:50 +0530

    setup db connection files

--- db/db.connection.js
+++ db/db.connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const mongodbUrl = process.env.connectionString;
 mongoose.set("strictQuery", false);
-mongoose.connect(mongodbUrl, { useNewUrlParser: true})
+mongoose.connect(mongodbUrl, { useNewUrlParser: true,dbName:"baap-acadamic-prod" })
 	.then(() => {
         console.log("Connected to the database");
 	})
