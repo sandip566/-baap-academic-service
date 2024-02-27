280c1d3a4be393a1131aa2a0abb0afb68de1f7a4
Akshada khatode <akshadakhatode@gmail.com>
2024-02-27 09:59:47 +0530

    added all code

--- schema/courses.schema.js
+++ schema/courses.schema.js
@@ -11,7 +11,7 @@ const courseSchema = new mongoose.Schema(
             required: false,
             type: String
         },
-        departmentId: {
+        Department: {
             type: Number,
             required: false
         },
