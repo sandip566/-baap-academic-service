f6717022f34248cdf6dd920f8463bbc1cdd5eab1
Akshada khatode <akshadakhatode@gmail.com>
2024-02-27 18:02:57 +0530

    added bulk upload

--- db/db.connection.js
+++ db/db.connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const mongodbUrl = process.env.connectionString;
 mongoose.set("strictQuery", false);
-mongoose.connect(mongodbUrl, { useNewUrlParser: true })
+mongoose.connect(mongodbUrl, { useNewUrlParser: true,dbName:"baap-acadamic-dev" })
 	.then(() => {
         console.log("Connected to the database");
 	})
