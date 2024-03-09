243962e547b4f4a17caf1ff8d0b79438cc1999f3
sakshidighe2004 <sakshidighe2004@gmail.com>
2024-03-09 15:27:09 +0530

    added purchase model

---
+++ schema/purchase.schema.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const PurchaseSchema = new mongoose.Schema(
+    {
+        groupId: {
+            type: Number,
+            required: false
+        },
+        purchaseId: {
+            type: Number
+        },
+        vendorId: {
+            type: Number,
+            required: false
+        },
+        customerId: {
+            type: Number,
+            required: false
+        },
+        purchaseDate: {
+            type: Date,
+            default: Date.now()
+        },
+        totalAmount: {
+            type: Number
+        },
+        paymentMethod: {
+            type: String,
+            enum: ["upi", "cash", "cheque"]
+        },
+        items: [{
+            book: {
+                type: String
+            },
+            quantity: {
+                type: Number,
+                required: false
+            },
+            unitPrice: {
+                type: Number,
+                required: false
+            }
+        }],
+        orderStatus: {
+            type: String,
+            enum: ["Delivered", "Shipped", "Pending","Canceled","Processing"]
+        }
+    },
+    { strict:false,timestamps: true }
+);
+
+const PurchaseModel = mongoose.model("purchase", PurchaseSchema);
+module.exports = PurchaseModel;
