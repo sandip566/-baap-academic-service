9c0cf2645c5d631291cac94fe7f3c355642fbeb5
kandalkarpriyanka99 <kandalkarpriyanka99@.com>
2024-03-09 15:46:29 +0530

    add publisher api

---
+++ services/publisher.service.js
@@ -0,0 +1,83 @@
+const { query } = require("express");
+const BaseService = require("@baapcompany/core-api/services/base.service");
+const publisherModel = require("../schema/publisher.schema");
+
+
+class publisherService extends BaseService {
+    constructor(dbModel, entityName) {
+        super(dbModel, entityName);
+    }
+    async getAllDataByGroupId(groupId, criteria, skip, limit) {
+        try {
+            const searchFilter = {
+                groupId: groupId,
+            };
+
+            if (criteria.search) {
+                const numericSearch = parseInt(criteria.search);
+                if (!isNaN(numericSearch)) {
+                    searchFilter.$or = [
+                        { publisherId: numericSearch },
+                       {phoneNumber:numericSearch}
+
+                    ];
+                } else {
+                    searchFilter.$or = [
+                        {
+                            name: {
+                                $regex: new RegExp(criteria.search, "i"),
+                            },
+                        },
+                        { address: { $regex: new RegExp(criteria.search, "i") } },
+                        {
+                            website: {
+                                $regex: new RegExp(criteria.search, "i"),
+                            },
+                        },
+                        {
+                          
+                             email : { $regex:new RegExp(criteria.search, "i")}
+                        
+                    }
+
+                    ];
+                }
+            }
+          if(criteria.phoneNumber){
+           searchFilter.phoneNumber=criteria.phoneNumber
+          }
+            if (criteria.name) {
+                searchFilter.name = {
+                    $regex: new RegExp(criteria.name, "i"),
+                };
+            }
+            
+            return { searchFilter };
+        } catch (error) {
+            console.error("Error in getAllDataByGroupId:", error);
+            throw new Error("An error occurred while processing the request.");
+        }
+    }
+
+    async deletePublisherById(publisherId, groupId) {
+        try {
+            return await publisherModel.deleteOne(publisherId, groupId);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updatePublisherById(publisherId, groupId, newData) {
+        try {
+            const updatePublisher = await publisherModel.findOneAndUpdate(
+                { publisherId: publisherId, groupId: groupId },
+                newData,
+                { new: true }
+            );
+            return updatePublisher;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+module.exports = new publisherService(publisherModel, "publisher");
