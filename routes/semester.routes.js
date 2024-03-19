1d1002c7f17e76e16e53d05b25cd10476763a090
Akshada khatode <akshadakhatode@gmail.com>
2024-03-19 18:28:06 +0530

    added all account senarios

---
+++ routes/semester.routes.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const { checkSchema } = require("express-validator");
+const service = require("../services/semester.service");
+const requestResponsehelper = require("@baapcompany/core-api/helpers/requestResponse.helper");
+const ValidationHelper = require("@baapcompany/core-api/helpers/validation.helper");
+
+router.post(
+    "/",
+    checkSchema(require("../dto/semester.dto")),
+    async (req, res, next) => {
+        if (ValidationHelper.requestValidationErrors(req, res)) {
+            return;
+        }
+        const serviceResponse = await service.create(req.body);
+        requestResponsehelper.sendResponse(res, serviceResponse);
+    }
+);
+
+router.delete("/:id", async (req, res) => {
+    const serviceResponse = await service.deleteById(req.params.id);
+
+    requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+router.put("/:id", async (req, res) => {
+    const serviceResponse = await service.updateById(req.params.id, req.body);
+
+    requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+router.get("/:id", async (req, res) => {
+    const serviceResponse = await service.getById(req.params.id);
+
+    requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+router.get("/all/semester", async (req, res) => {
+    const serviceResponse = await service.getAllByCriteria({});
+
+    requestResponsehelper.sendResponse(res, serviceResponse);
+});
+router.get("/all/getByGroupId/:groupId", async (req, res) => {
+    const groupId = req.params.groupId;
+    const criteria = {
+        name: req.query.name,
+      
+    };
+    const serviceResponse = await service.getAllDataByGroupId(
+        groupId,
+        criteria
+    );
+    requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+
+module.exports = router;
