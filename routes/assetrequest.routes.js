69ad9a50ac5c9a7f2dcc11798d3a63d7698e2661
shubhamGitLab0005 <shubhamgavhanebaap@gmail.com>
2024-04-16 00:14:04 +0530

    added asset management api

---
+++ routes/assetrequest.routes.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const router = express.Router();
+const { checkSchema } = require("express-validator");
+const service = require("../services/assetrequest.service");
+const requestResponsehelper = require("@baapcompany/core-api/helpers/requestResponse.helper");
+const ValidationHelper = require("@baapcompany/core-api/helpers/validation.helper");
+
+router.post(
+  "/",
+  checkSchema(require("../dto/assetrequest.dto")),
+  async (req, res, next) => {
+    if (ValidationHelper.requestValidationErrors(req, res)) {
+      return;
+    }
+    const requestId = +Date.now();
+    req.body.requestId = requestId;
+    const serviceResponse = await service.create(req.body);
+    requestResponsehelper.sendResponse(res, serviceResponse);
+  }
+);
+
+router.delete("/:id", async (req, res) => {
+  const serviceResponse = await service.deleteById(req.params.id);
+  requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+router.put("/:id", async (req, res) => {
+  const serviceResponse = await service.updateById(req.params.id, req.body);
+  requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+router.get("/:id", async (req, res) => {
+  const serviceResponse = await service.getById(req.params.id);
+  requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+router.get("/all/assetRequest", async (req, res) => {
+  const serviceResponse = await service.getAllByCriteria({});
+  requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+router.get("/all/getByGroupId/:groupId", async (req, res) => {
+  const groupId = req.params.groupId;
+  const criteria = {
+    name: req.query.name,
+    type: req.query.type,
+    status: req.query.status,
+    category: req.query.category,
+    pageNumber: parseInt(req.query.pageNumber) || 1,
+    pageSize: parseInt(req.query.pageSize) || 10,
+  };
+  const serviceResponse = await service.getAllDataByGroupId(
+    groupId,
+    criteria
+  );
+  requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+
+router.get("/getByRequestId/:id", async (req, res) => {
+  const serviceResponse = await service.getByDataId(req.params.id);
+  requestResponsehelper.sendResponse(res, serviceResponse);
+});
+
+router.delete("/groupId/:groupId/requestId/:requestId", async (req, res) => {
+  try {
+    const requestId = req.params.requestId;
+    const groupId = req.params.groupId;
+    const Data = await service.deleteByDataId(requestId, groupId);
+    if (!Data) {
+      res.status(404).json({ error: "Data not found to delete" });
+    } else {
+      res.status(201).json(Data);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.put("/groupId/:groupId/requestId/:requestId", async (req, res) => {
+  const requestId = req.params.requestId;
+  const groupId = req.params.groupId;
+  const updateData = req.body;
+  try {
+    const serviceResponse = await service.updateDataById(requestId, groupId, updateData);
+    if (serviceResponse) {
+      const response = { data: serviceResponse, message: "Data updated successfully" };
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({ error: "Data not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+module.exports = router;
