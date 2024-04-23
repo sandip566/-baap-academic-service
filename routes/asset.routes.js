25bb21393dd182c760a5f86a7bfa1726b816bc53
kandekaryash33 <kandekaryash33@gmail.com>
2024-04-23 11:20:18 +0530

    Changes in asset module

--- routes/asset.routes.js
+++ routes/asset.routes.js
@@ -19,6 +19,54 @@ router.post(
     }
 );
 
+router.get("/all/getByGroupId/:groupId", async (req, res) => {
+    const groupId = req.params.groupId;
+    const criteria = {
+      assetId: req.query.assetId,
+      assetName: req.query.assetName,
+      location: req.query.location,
+      status: req.query.status,
+      assetType:req.query.assetType
+    };
+    const serviceResponse = await service.getAllDataByGroupId(
+      groupId,
+      criteria
+    );
+    requestResponsehelper.sendResponse(res, serviceResponse);
+  });
+  
+  router.put("/groupId/:groupId/assetId/:assetId", async (req, res) => {
+    try {
+      const assetId = req.params.assetId;
+      const groupId = req.params.groupId;
+      const newData = req.body;
+      const data = await service.updateByAssetId(assetId, groupId, newData);
+      if (!data) {
+        res.status(404).json({ error: 'Asset not found to update' });
+      } else {
+        res.status(201).json(data);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  router.delete("/groupId/:groupId/assetId/:assetId", async (req, res) => {
+    try {
+      const assetId = req.params.assetId;
+      const groupId = req.params.groupId;
+      const data = await service.deleteByAssetId(assetId, groupId);
+      if (!data) {
+        res.status(404).json({ error: 'Asset not found to delete' });
+      } else {
+        res.status(201).json(data);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 router.delete("/:id", async (req, res) => {
     const serviceResponse = await service.deleteById(req.params.id);
 
