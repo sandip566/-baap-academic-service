74ce759bc1d6263b64f149ccd2baffbd122e8fbd
nynougale8@gmail.com <nynougale8@gmail.com>
2024-05-18 15:47:59 +0530

    worked on traveller and routes api

--- schema/traveller.schema.js
+++ schema/traveller.schema.js
@@ -2,70 +2,40 @@ const mongoose = require("mongoose");
 
 const TravellerSchema = new mongoose.Schema(
     {
-        groupId:{
-            type:Number,
-            required:true
+        groupId: {
+            type: Number,
+            required: true
         },
-        travellerName:{
-            type:String,
-            required:true
+        travellerId: {
+            type: Number,
+            required: true
         },
-        travellerId:{
-            type:Number,
-            required:true
+        empId: {
+            type: Number,
+            required: true
         },
-        phoneNumber:{
-            type:Number,
-            required:true
-        },
-        class:{
-            type:String,
-            required:true
-        },
-        division:{
-            type:String,
-            required:true
-        },
-        email:{
-            type:String,
-            required:true
-        },
-        ParentsName:{
-            type:String,
-            required:true
-        },
-        birthDate:{
-            type:String,
-            required:true
-        },
-        address:[{
-            village: {
-                type: String,
-                required: true
-            },
-            city: {
-                type: String,
-                required: true
-            },
-            state: {
-                type: String,
-                required: true
-            }
-        }],
-        country:{
+        startDate: {
             type: String,
+            required: true
         },
-        KYC:[{
-            aadharNumber: {
-                type: Number,
-                required: true
-            },
-            panNumber: {
-                type: String,
-                required: true
-            }
-        }]
-},
+        endDate: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['Draft', 'Comfirmed', 'Cancelled'],
+            required: true
+        },
+        stopId: {
+            type: Number,
+            required: true
+        },
+        totalFees: {
+            type: Number,
+            required: true
+        }
+    },
     { timestamps: true }
 );
 
