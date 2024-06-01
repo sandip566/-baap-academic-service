378f7816afed0b2e397cfd27064f7a7b8de3eaa6
Akshada khatode <akshadakhatode@gmail.com>
2024-06-01 15:13:52 +0530

    added changes related hostel and rooms populate

--- services/rooms.services.js
+++ services/rooms.services.js
@@ -6,9 +6,15 @@ class Service extends BaseService {
         super(dbModel, entityName);
     }
 
-    async getAllRoomDataByGroupId(groupId, criteria) {
+    async getAllRoomDataByGroupId(
+        groupId,
+        criteria,
+        page,
+        limit,
+        reverseOrder = true
+    ) {
         const query = {
-            groupId: groupId,
+            groupId: Number(groupId),
         };
         criteria.pageSize = 10;
         if (criteria.roomId) query.roomId = criteria.roomId;
@@ -16,8 +22,43 @@ class Service extends BaseService {
         if (criteria.hostelId) query.hostelId = criteria.hostelId;
         if (criteria.status) query.status = new RegExp(criteria.status, "i");
         if (criteria.name) query.name = new RegExp(criteria.name, "i");
+        const currentPage = page;
+        const perPage = limit;
+        const skip = (currentPage - 1) * perPage;
 
-        return this.preparePaginationAndReturnData(query, criteria);
+        let data = await roomModel
+            .aggregate([
+                { $match: query },
+                {
+                    $lookup: {
+                        from: "hostelpremises",
+                        localField: "hostelId",
+                        foreignField: "hostelId",
+                        as: "hostelId",
+                    },
+                },
+                {
+                    $unwind: {
+                        path: "$hostelId",
+                        preserveNullAndEmptyArrays: true,
+                    },
+                },
+                { $sort: { createdAt: reverseOrder ? -1 : 1 } },
+                { $skip: skip },
+                { $limit: perPage },
+            ])
+            .exec();
+
+        const totalItemsCount = await roomModel.countDocuments(query);
+
+        const response = {
+            status: "Success",
+            data: {
+                items: data,
+                totalItemsCount: totalItemsCount,
+            },
+        };
+        return response;
     }
 
     async deleteRoomById(roomId, groupId) {
