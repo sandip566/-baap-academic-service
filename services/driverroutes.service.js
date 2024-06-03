376be0812b59530d355bd2fc986ef0e884ec3754
nynougale8@gmail.com <nynougale8@gmail.com>
2024-06-03 12:05:26 +0530

    worked on driverRoute and travellerStop

---
+++ services/driverroutes.service.js
@@ -0,0 +1,45 @@
+const DriverRoutesModel = require("../schema/driverroutes.schema");
+const BaseService = require("@baapcompany/core-api/services/base.service");
+const BusRoutesModel = require('../schema/busroutes.schema');
+const DriverModel = require('../schema/driver.schema')
+class DriverRoutesService extends BaseService {
+    constructor(dbModel, entityName) {
+        super(dbModel, entityName);
+    }
+
+    async getByBusRouteId(groupId, driverRouteId) {
+        console.log("query",{ groupId: groupId, driverRouteId: driverRouteId });
+        let driverRoutes = await DriverRoutesModel.findOne({ groupId: Number(groupId), driverRouteId: Number(driverRouteId) })
+        console.log("driverRoutes",driverRoutes);
+        const routeId = driverRoutes.routeId
+        console.log("data", routeId);
+        if (!routeId) {
+            throw new Error("routeId not found.");
+        }
+        let route = await BusRoutesModel.findOne({ groupId: groupId, routeId: routeId });
+
+        const driverId = route.driverId
+        if (!driverId) {
+            throw new Error("driverId not found.");
+        }
+        let driver = await DriverModel.findOne({ groupId: groupId, driverId: driverId });
+
+        let responseData = {
+            status: "Success",
+            data: {
+                driverRoutes: {
+                    ...driverRoutes.toObject(), 
+                    routeId: route.toObject(),
+                    driverId: driver.toObject()
+                }
+            }
+        };
+
+        return responseData;
+    }
+
+
+
+}
+
+module.exports = new DriverRoutesService(DriverRoutesModel, 'driverroutes');
---
+++ services/travellerstop.service.js
@@ -0,0 +1,14 @@
+const TravellerStopModel = require("../schema/travellerstop.schema");
+const BaseService = require("@baapcompany/core-api/services/base.service");
+const BusRoutesModel = require("../schema/busroutes.schema");
+const driverModel = require("../schema/driver.schema");
+
+
+class TravellerStopService extends BaseService {
+    constructor(dbModel, entityName) {
+        super(dbModel, entityName);
+    }
+
+}
+
+module.exports = new TravellerStopService(TravellerStopModel, 'travellerstop');
